1147b198c19a2856c540c4bbc70b595d835516f1
Miha Žnidar <mayki.znidar@gmail.com>
2022-12-26 17:58:58 +0100

    Remove unused files

--- out/_next/static/g-L3mw1DVFSCoJxYIVw_E/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5e2202f8a4714493.css","static/chunks/pages/index-fa9c7b21792df490.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
