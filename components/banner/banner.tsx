ff6ecbe0253acba672efe51f512c0a1293f2fc98
Miha Žnidar <mayki.znidar@gmail.com>
2023-01-06 11:40:28 +0100

    New banner

--- components/banner/banner.tsx
+++ components/banner/banner.tsx
@@ -4,8 +4,11 @@ import styles from './banner.module.scss';
 export const Banner: NextComponentType = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.scrollingText}>
-        <p>LET'S <span>TALK</span></p>
+      <div className={styles.marquee}>
+        <span className={styles.text}>LET'S <span className={styles.purple}>TALK</span>&nbsp;&nbsp;</span>
+      </div>
+      <div className={styles.marquee}>
+        <span className={`${styles.text} ${styles.delay}`}>LET'S <span className={styles.purple}>TALK</span>&nbsp;&nbsp;</span>
       </div>
     </div>
   )
