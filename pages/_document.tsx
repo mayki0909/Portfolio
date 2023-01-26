28ceec2604015f8125c9ee956bed6e59509163f1
Miha Žnidar <mayki.znidar@gmail.com>
2023-01-26 13:50:23 +0100

    Ga in head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,6 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html lang="en">
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-T2RSCD3EPM"></Script>
-      <Script>{`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-T2RSCD3EPM');
-      `}</Script>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -21,7 +14,14 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         {/* INTER FONT */}
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" /> 
-      
+        
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-T2RSCD3EPM"></Script>
+        <Script>{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-T2RSCD3EPM');
+        `}</Script>
       </Head>
       <body>
         <Main />
