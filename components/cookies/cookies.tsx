2715abded3c5f512d7a2bc1f3ef9a89432259e9b
Miha Žnidar <mayki.znidar@gmail.com>
2023-01-26 16:57:58 +0100

    GA Tracker in cookies component

--- components/cookies/cookies.tsx
+++ components/cookies/cookies.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import { useEffect, useState } from 'react'
 import styles from './cookies.module.scss'
 
@@ -15,9 +16,18 @@ export const Cookies = () => {
   }, [])
 
   return (
-    <div className={`${styles.modal} ${accepted ? 'd-none' : 'd-inline d-md-flex'}`}>
-      <p>This site tracks visits anonymously using cookies. Close this dialog to confirm you are happy with that.</p>
-      <button onClick={()=>accpetCookies()}>Aggree and close</button>
-    </div>
+    <>        
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-T2RSCD3EPM"></Script>
+      <Script>{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-T2RSCD3EPM');
+      `}</Script>
+      <div className={`${styles.modal} ${accepted ? 'd-none' : 'd-inline d-md-flex'}`}>
+        <p>This site tracks visits anonymously using cookies. Close this dialog to confirm you are happy with that.</p>
+        <button onClick={()=>accpetCookies()}>Aggree and close</button>
+      </div>
+    </>
   )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -14,14 +13,6 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         {/* INTER FONT */}
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" /> 
-        
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-T2RSCD3EPM"></Script>
-        <Script>{`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-T2RSCD3EPM');
-        `}</Script>
       </Head>
       <body>
         <Main />
