843665a87582bdf93f982487d6a73a08b9ff93fb
mayki0909 <mayki.znidar@gmail.com>
2026-05-22 23:19:12 +0200

    Updated version

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
+      use: ["@svgr/webpack"],
     });
     return config;
   },
 };
+
+module.exports = nextConfig;
